7e5a62cb6e728bb9397ec57bcb62010fc21f96b3
jieonxei <reixeiju@gmail.com>
2026-06-01 09:58:23 +0800

    Added explanation per line of codes

--- Practice-Python-Exercises/exercise-1.py
+++ Practice-Python-Exercises/exercise-1.py
@@ -7,12 +7,21 @@
 # Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
 # Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
 
-from datetime import datetime
+# imports the datetime module to get the current year
+from datetime import datetime  
 
+# Ask the user for their name
 name = input("What is your name? ")
+
+# Ask the user for their age and convert it to an integer
 age = int(input("How old are you? "))
 
+# Get the current year automatically
 current_year = datetime.now().year
+
+# Calculate the year when the user will turn 100
+# (100 - age) gives how many years are left until 100
 year_for_100 = current_year + (100 - age)
 
-print (name,", you will turn 100 years old in", year_for_100)
+# Print the final result
+print(name, ", you will turn 100 years old in", year_for_100)
