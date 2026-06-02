8741afe52940a77d15e9cb2813e03a1a5da39af3
jieonxei <reixeiju@gmail.com>
2026-06-02 09:25:09 +0800

    Completed Exercise 1 - Arithmetic Product and Conditional Logic

--- PYnatives-Exercises/Basic-Exercise-for-Beginners/1 Arithmetic Product and Conditional Logic.py
+++ PYnatives-Exercises/Basic-Exercise-for-Beginners/1 Arithmetic Product and Conditional Logic.py
@@ -1,1 +1,19 @@
+# Arithmetic Product and Conditional Logic
+# Exercise 1
+# Write a Python function that accepts two integer numbers. 
+# If the product of the two numbers is less than or equal to 1000, return their product; otherwise, return their sum.
+
+# defines a function named calculate (it accepts the number1&2)
+def calculate(number1, number2):
+    #then compute the two products
+    product = number1 * number2
+
+    #if the product is less than a thousand it will fall or return to the argument (+)
+    if product <= 1000:
+        return product
+    else:
+        return number1 + number2
+
+print(calculate(20,30))
+print(calculate(30,70))
 
