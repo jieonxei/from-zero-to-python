d7adb247a6e8c524ac35f60bbf561a5f3da8eddc
jieonxei <reixeiju@gmail.com>
2026-06-04 09:23:41 +0800

    Completed Exercise 7 - List Manipulation

---
+++ PYnatives-Exercises/Basic-Exercise-for-Beginners/7 List Manipulation.py
@@ -0,0 +1,13 @@
+# Exercise 7
+# List Manipulation
+# Create a list of 5 fruits. Add a new fruit to the end of the list, then remove the second fruit (at index 1).
+
+fruits = ["apple", "banana", "cherry", "strawberry", "blueberry"]
+
+# to add use the "append"
+fruits.append("mango")
+
+# to remove use "pop"
+fruits.pop(1)
+
+print(fruits)
